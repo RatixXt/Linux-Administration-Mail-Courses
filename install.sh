7b5a0914038fbf9c4779c2a1ec8a2914dc57d83b
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-12-17 18:54:58 +0300

    Добавлены ;

--- install.sh
+++ install.sh
@@ -14,17 +14,17 @@ exec()
 	fi
 }
 
-exec "apt install sysstat"
-exec "apt-get install nginx"
-exec "cp ./conf/nginx/balinux /etc/nginx/sites-available/"
-exec "ln -s /etc/nginx/sites-available/balinux /etc/nginx/sites-enabled/balinux"
-exec "apt-get install apache2"
-exec "cp ./conf/apache2/ports.conf /etc/apache2/"
-exec "cp ./conf/apache2/bonet1.conf /etc/apache2/sites-available/"
-exec "a2ensite bonet1"
+exec "apt install sysstat";
+exec "apt-get install nginx";
+exec "cp ./conf/nginx/balinux /etc/nginx/sites-available/";
+exec "ln -s /etc/nginx/sites-available/balinux /etc/nginx/sites-enabled/balinux";
+exec "apt-get install apache2";
+exec "cp ./conf/apache2/ports.conf /etc/apache2/";
+exec "cp ./conf/apache2/bonet1.conf /etc/apache2/sites-available/";
+exec "a2ensite bonet1";
 # Возможно надо будет удалять дефолтные сайты
-exec "mkdir -p /var/vhosts/bonet1"
-exec "cp -r ./bonet1 /var/vhosts/"
-exec "chown www-data:www-data -R /var/vhosts/bonet1/data"
-exec "service apache2 restart"
-exec "service nginx restart"
+exec "mkdir -p /var/vhosts/bonet1";
+exec "cp -r ./bonet1 /var/vhosts/";
+exec "chown www-data:www-data -R /var/vhosts/bonet1/data";
+exec "service apache2 restart";
+exec "service nginx restart";
